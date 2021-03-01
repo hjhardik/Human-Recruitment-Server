72949f84a1b5059764c2b5dd7796091340331807
Hardik Joshi <hardikj7321@gmail.com>
2021-03-01 22:15:41 +0530

    changed url body

--- server.js
+++ server.js
@@ -387,9 +387,7 @@ app.post('/signauth/redirect', async (req,res) => {
     })
   })
   }else{
-    let url = `https://secure.na1.adobesign.com/public/oauth?redirect_uri=${redirectUrl}&
-    response_type=code&client_id=${clientID}&scope=user_login:self+agreement_read:self+
-    agreement_write:self+agreement_send:self&state=S6YQD7KDA556DIV6NA`;
+    let url = `https://secure.na1.adobesign.com/public/oauth?redirect_uri=${redirectUrl}&response_type=code&client_id=${clientID}&scope=user_login:self+agreement_read:self+agreement_write:self+agreement_send:self&state=S6YQD7KDA556DIV6NA`;
     res.json({
       success: true,
       data: url,
