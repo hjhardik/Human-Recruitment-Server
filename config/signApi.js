65c635ad6b4e4bf10f3fabf4c036138c6e2143d1
Hardik Joshi <hardikj7321@gmail.com>
2021-03-01 16:25:09 +0530

    changed redirect url

--- config/signApi.js
+++ config/signApi.js
@@ -1,4 +1,4 @@
-redirectURL = "https://hjhardik.github.io/human-recruitment/signcontract"
+redirectURL = "https://human-recruitment.herokuapp.com/signcontract"
 clientID = "CBJCHBCAABAAN4Co_y_LKH5ZZrs7FZ1LHfibg_NMQ5Au"
 clientSecret = "ooryrja9VBwOoZuKv2nnyw0yd9qfubeG"
 
