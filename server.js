6c92c7ad8eb3e71a98b7a21f96c1cd796c5001fc
Hardik Joshi <hardikj7321@gmail.com>
2021-03-01 17:22:04 +0530

    added access control snippet in server.js

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //DB Config
 const db = require("./config/keys").mongoURI;
