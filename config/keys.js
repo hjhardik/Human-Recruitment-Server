5f0dd0c3d6a469c9f9ac927c64353897f0a6c471
Hardik Joshi <hardikj7321@gmail.com>
2021-10-05 19:15:28 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 //dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 //mongodb+srv://Hardik:<password>@cluster0.wm86f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 dbPassword =
-  "mongodb+srv://Hardik:Hardik@cluster0.wm86f.mongodb.net/test?retryWrites=true&w=majority";
+  mongodb+srv://Hardik:<password>@cluster0.wm86f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 //export the database URI
 module.exports = {
