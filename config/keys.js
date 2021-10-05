22c453319e4aad41cbe5b7935ac16cfbb41cb5e6
Hardik Joshi <hardikj7321@gmail.com>
2021-10-05 19:08:52 +0530

    Changed the dbPassword field

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,8 @@
 //If want to use your own MONGODB database update the db password as follows:
 //dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+//mongodb+srv://Hardik:<password>@cluster0.wm86f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 dbPassword =
-  "mongodb+srv://Hardik:Hardik@cluster0.wm86f.mongodb.net/test";
+  "mongodb+srv://Hardik:Hardik@cluster0.wm86f.mongodb.net/test?retryWrites=true&w=majority";
 
 //export the database URI
 module.exports = {
